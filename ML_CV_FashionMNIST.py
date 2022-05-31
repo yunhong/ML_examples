c78bf213d56cbdf32b41ce44f775bcdcc6adb3f5
yunhong <yunhongz@outlook.com>
2022-05-30 18:03:40 -0700

    Update ML_CV_FashionMNIST.py

--- ML_CV_FashionMNIST.py
+++ ML_CV_FashionMNIST.py
@@ -13,7 +13,8 @@ model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
 model.compile(optimizer = 'adam',
-              loss = 'sparse_categorical_crossentropy')
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 model.fit(training_images, training_labels, epochs=5)
 
