3ae950f9428173cba139fb5902dccd40647f7dca
yunhong <yunhongz@outlook.com>
2022-06-05 17:45:23 -0700

    Update RNNs.py

--- RNNs.py
+++ RNNs.py
@@ -15,3 +15,24 @@ for module in mpl, np, pd, sklearn, tf, keras:
 
 assert sys.version_info >= (3, 5) # Python ≥3.5 required
 assert tf.__version__ >= "2.0"    # TensorFlow ≥2.0 required
+
+dataset_path = keras.utils.get_file(
+    "daily-minimum-temperatures-in-me.csv",
+    "https://raw.githubusercontent.com/ageron/tf2_course/master/datasets/daily-minimum-temperatures-in-me.csv")
+    
+temps = pd.read_csv(dataset_path,
+                    parse_dates=[0], index_col=0)
+
+temps.info()
+
+temps.head()
+
+temps.plot(figsize=(10,5))
+plt.show()
+
+temps.plot(figsize=(10,5))
+plt.show()
+
+temps = temps.asfreq("1D", method="ffill")
+temps.loc["1984-12-29":"1985-01-02"]
+
